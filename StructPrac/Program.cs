896f7ac5b422afcf82c4ff5e39d9192e36cea2c7
Insori <s2110@e-mirim.hs.kr>
2023-09-04 14:38:28 +0900

    구조체 생성자 실습

--- StructPrac/Program.cs
+++ StructPrac/Program.cs
@@ -8,8 +8,37 @@ namespace StructPrac
 {
     internal class Program
     {
+        struct Point
+        {
+            public int x;
+            public int y;
+            public string testA;
+            public string testB;
+
+            public Point(int number)
+            {
+                x = number;
+                y = number;
+                testA = testB = null;
+            }
+
+            public Point(int a, int b)
+            {
+                x = a;
+                y = b;
+                testA = "에이";
+                testB = "비이";
+            }
+        }
         static void Main(string[] args)
         {
+            Point p;
+            p.x = 10;
+            p.y = 20;
+            Console.WriteLine($"{p.x} - {p.y}");
+
+            Point p2 = new Point();
+            Console.WriteLine($"{p2.x} - {p2.y}");
         }
     }
 }
